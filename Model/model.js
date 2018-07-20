1f7ebeb5d4dcecfce131f3fa063487134a6efd0f
Fajar Tri Cahyono <fajartc02@gmail.com>
2018-07-20 16:34:23 +0700

    completed hospital-interface all release

--- Model/model.js
+++ Model/model.js
@@ -1,22 +1,165 @@
+const fs = require('fs')
+
+
 class Patient {
   constructor(id, name, diagnosis) {
-    this.id = id
-    this.name = name
-    this.diagnosis = diagnosis
+    this._id = id
+    this._name = name
+    this._diagnosis = diagnosis
   }
 }
 
 class Employee {
-  constructor(name, position, username, password) {
-    this.name = name
-    this.position = position
-    this.username = username
-    this.password = password
+  constructor(username, password, role) {
+    this._username = username
+    this._password = password
+    this._role = role
+    this._status = false
   }
+
+  get name() {
+    return this._name
+  }
+
+  get password() {
+    return this._password
+  }
+
+  get role() {
+    return this._role
+  }
+
+  get status() {
+    return this._status
+  }
+
+  set status(state) {
+    this._status = state
+  }
+}
+
+class Hospital {
+  constructor() {
+    this._employees = null,
+    this._patients = null
+  }
+
+  get employees() {
+    return this._employees
+  }
+
+  set employees(newEmployee) {
+    this._employees = newEmployee
+  }
+
+  get patients() {
+    return this._patients
+  }
+
+  set patients(newPatient) {
+    this._patients = newPatient
+  }
+
+  readFileEmployee(cb) {
+    fs.readFile('employee.json', 'utf8', (err, data) => {
+      (!err) ? this.employees = JSON.parse(data) : console.log(err);
+      cb()
+    })
+  }
+
+  readFilePatient(cb) {
+    fs.readFile('patient.json', 'utf8', (err, data) => {
+      (!err) ? this.patients = JSON.parse(data) : console.log(err);
+      cb()
+    })
+  }
+
+  writeFile(newData) {
+    fs.writeFile('employee.json', JSON.stringify(newData), (err) => {
+      (err) ? console.log(err) : 'Success';
+    })
+  }
+
+  writeFilePatient(newData) {
+    fs.writeFile('patient.json', JSON.stringify(newData), (err) => {
+      (err) ? console.log(err) : 'Success';
+    })
+  }
+
+
+  registerEmployee(username, password, role, callback) {
+    this.readFileEmployee(() => {
+      let newEmployee = new Employee(username, password, role)
+      this.employees.push(newEmployee)
+      this.writeFile(this.employees)
+      callback()
+    })
+  }
+
+  loginEmployee(username, password, callback) {
+    this.readFileEmployee(() => {
+      let condition = false
+      this.employees.forEach(employee => {
+        // console.log(employee._username); // tandain tanya
+        if(employee._username === username && employee._password === password) {
+          condition = true
+          employee._status = true
+          this.writeFile(this.employees)
+          callback(employee._username, condition)
+        }
+      })
+      if(condition === false) {
+        callback(condition)
+      }
+    })
+  }
+
+  logoutEmployee(username, callback) {
+    this.readFileEmployee(() => {
+      let condition = false
+      this.employees.forEach(employee => {
+        if(employee._username === username) {
+          condition = true
+          employee._status = false
+          this.writeFile(this.employees)
+          callback(employee._username, condition)
+        }
+      })
+      if(condition === false) {
+        callback(null, condition)
+      }
+    })
+  }
+
+  addPatient(patient, diagnosis, callback) {
+    this.readFileEmployee(() => {
+      let condition = false
+      this.employees.forEach(employee => {
+        if(employee._role === 'dokter' && employee._status === true) {
+          this.readFilePatient(() => {
+            console.log(this.patients)
+            let id = this.patients.length
+            //this for check id 
+            id = (id === 0) ?  1 :  this.patients.length + 1
+            let newPatient = new Patient(id ,patient, diagnosis)
+            this.patients.push(newPatient)
+            this.writeFilePatient(this.patients)
+            condition = true
+            callback(condition)
+          })
+        }
+      })
+      if(condition === false) {
+        callback(condition)
+      }
+    })
+  }
+
 }
 
 
 module.exports = {
   Patient: Patient,
-  Employee:Employee
+  Employee: Employee,
+  Hospital: Hospital
 }
--- index.js
+++ index.js
@@ -1,2 +1,47 @@
-const Controller = require('./Controller/controller')
-const argv = process.argv
+const controller = require('./Controller/controller')
+const argv = process.argv.slice(2)
+const command = argv[0]
+const input = argv
+const Controller = new controller()
+
+// Controller.startAll(()=> {
+
+// })
+
+if(command === undefined) {
+    Controller.helpMenu()
+} else if (command === 'help') {
+    Controller.menu()
+} else if(command === 'register') {
+    if(input[1] !== undefined && input[2] !== undefined && input[3] !== undefined) {
+        let username = input[1];
+        let password = input[2];
+        let role = input[3]
+        Controller.registerEmployee(username, password, role)
+    } else {
+        Controller.menu()
+    }
+} else if (command === 'login') {
+    if(input[1] !== undefined && input[2] !== undefined) {
+        let username = input[1];
+        let password = input[2];
+        Controller.loginEmployee(username, password)
+    } else {
+        Controller.menu()
+    }
+} else if (command === 'addPatient') {
+    if(input[1] !== undefined && input[2] !== undefined) {
+        let patient = input[1]
+        let diagnosis = input.slice(2)
+        Controller.addPatient(patient, diagnosis)
+    } else {
+        Controller.menu()
+    }
+} else if(command === 'logout') {
+    if(input[1] !== undefined) {
+        let username = input[1]
+        Controller.logoutEmployee(username)
+    } else {
+        Controller.menu()
+    }
+}
