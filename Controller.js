627398e8baf181d08a8c2dd5b8ab262f8f20474b
talkasrul <talkasrul@gmail.com>
2018-07-21 16:00:43 +0700

    Dokter only add patient

--- Controller.js
+++ Controller.js
@@ -5,6 +5,7 @@ class Controller {
     constructor(perintah) {
         this.perintah = perintah;
         this.employee = [];
+        this.patient = [];
         this.command();       
     }
 
@@ -19,6 +20,8 @@ class Controller {
             this.login();
         } else if(this.perintah[0] == 'logout') {
             this.logout();
+        } else if(this.perintah[0] == 'addPatient') {
+            this.addPatient();
         }
     }
 
@@ -28,22 +31,54 @@ class Controller {
         });
     }
 
+    addPatient() {
+        Model.ReadEmployee((data) => {
+            let docter = data.filter(e => {
+                return e.login > false
+            })
+            if(docter[0].role == 'docter') {
+                Model.ReadPatient( (dataPatient) => {
+                    this.patient = dataPatient;
+
+                    let id = dataPatient[dataPatient.length - 1].id + 1;
+                    let calender = new Date;
+                    let tgl = calender.toLocaleDateString();
+                    let newPatient = {
+                        "id": id,
+                        "name": this.perintah[1],
+                        "disease": [this.perintah[2]],
+                        "createdAt": tgl
+                    };
+                    this.patient.push(newPatient);
+                    Model.addPatient(this.patient);
+                    this.patient = [];
+                })
+            } else {
+                console.log("Anda Bukan Dokter")
+            }
+        });
+    }
+
     login(username = this.perintah[1], password = this.perintah[2]) {
         Model.ReadEmployee((data) => {
             let dataEmployee = data;
             var message = '';
             for(let i in dataEmployee) {
-                if(dataEmployee[i].login == true && dataEmployee[i].username == this.perintah[1]) {
-                    message = `Anda sedang login dengan user ${ this.perintah[1] }`
-                } else if(dataEmployee[i].login == true && dataEmployee[i].username != this.perintah[1]) {
-                    message = 'Maaf, Sedang ada user lain yang login'
+                if(dataEmployee[i].login == true && dataEmployee[i].username != this.perintah[1]) {
+                    message = 'Maaf, Sedang ada user lain yang login';
+                    this.logout()
+                    break;
+                } else if(dataEmployee[i].login == true && dataEmployee[i].username == this.perintah[1]) {
+                    message = `Anda sedang login dengan user ${ this.perintah[1] }`;
+                    break;
                 } else if(username == dataEmployee[i].username && password == dataEmployee[i].password) {
                     this.employee = dataEmployee;
                     this.employee[i].login = true;
                     Model.registerEmployee(this.employee);
                     message = `Selamat datang ${ dataEmployee[i].username } `
                     this.employee = [];
-                }
+                    break;
+                } 
             } View.DisplayLogin(message);
 
         });
--- Model.js
+++ Model.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
-class Patient {
-  constructor(id, name, diagnosis) {
-    this.id = id
-    this.name = name
-    this.diagnosis = diagnosis
-  }
-}
+// class Patient {
+//   constructor(id, name, diagnosis) {
+//     this.id = id
+//     this.name = name
+//     this.diagnosis = diagnosis
+//   }
+// }
 
-class Employee {
-  constructor(name, position, username, password) {
-    this.name = name
-    this.position = position
-    this.username = username
-    this.password = password
-  }
-}
+// class Employee {
+//   constructor(name, position, username, password) {
+//     this.name = name
+//     this.position = position
+//     this.username = username
+//     this.password = password
+//   }
+// }
 
 class Model {
 	static ReadEmployee(employeeHandler) {
@@ -29,7 +29,23 @@ class Model {
 
 	static registerEmployee(newEmployee) {
 		fs.writeFile('./employee.json', JSON.stringify(newEmployee, null, 4), (err) => {
+			
+		});
+	}
 
+	static addPatient(newPatient) {
+		fs.writeFile('./patients.json', JSON.stringify(newPatient, null, 4), (err) => {
+			
+		});
+	}
+
+	static ReadPatient(PatientHandler) {
+		fs.readFile('./patients.json', 'utf8', (err, data) => {
+			if(!err) {
+				PatientHandler(JSON.parse(data))
+			} else {
+				console.log("error")
+			}
 		});
 	}
 }
